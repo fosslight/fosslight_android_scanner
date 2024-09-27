16bbf4c7334ac9792a3bc250de1eb3fe60246f01
shinzz410 <tlswldnjs410@gmail.com>
2024-09-27 20:29:37 +0900

    Refactor test_release environment

--- test/pytest/test_tox.py
+++ test/pytest/test_tox.py
@@ -19,3 +19,21 @@ def test_fosslight_android(run_command, android_src_path, android_build_log):
 
     # then
     assert success is True, f"fosslight_android test_run failed. stderr: {stderr}"
+
+def test_release_environment(run_command):
+
+    # given
+    run_command("rm -rf test_result")
+    os.makedirs("test_result", exist_ok=True)
+
+    # when
+    help_result, _ = run_command("fosslight_android -h")
+    ok_result, _ = run_command("fosslight_android -b test/binary.txt -n test/NOTICE.html -c ok")
+    nok_result, _ = run_command("fosslight_android -b test/binary.txt -n test/NOTICE.html -c nok")
+    divide_result, _ = run_command("fosslight_android -d test/needtoadd-notice.html")
+
+    # then
+    assert help_result is True, "Help command failed"
+    assert ok_result is True, "OK command failed"
+    assert nok_result is True, "NOK command failed"
+    assert divide_result is True, "Divide command failed"
