3e0efc45702795316dc639ebf510adc3a7a85368
shinzz410 <tlswldnjs410@gmail.com>
2024-09-28 16:58:01 +0900

    Fix empty line

--- test/test_tox.py
+++ test/test_tox.py
@@ -5,32 +5,39 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import os
-import pytest
+
 
 def test_fosslight_android(run_command, android_src_path, android_build_log):
+
 
     # given
     assert android_src_path, "android_src_path is not set."
     assert android_build_log, "android_build_log is not set."
 
+
     # when
     command = f"fosslight_android -s {android_src_path} -a {android_build_log} -m"
     success, stdout, stderr = run_command(command)
 
+
     # then
     assert success is True, f"fosslight_android test_run failed. stderr: {stderr}"
 
+
 def test_release_environment(run_command):
+
 
     # given
     run_command("rm -rf test_result")
     os.makedirs("test_result", exist_ok=True)
+
 
     # when
     help_result, _ = run_command("fosslight_android -h")
     ok_result, _ = run_command("fosslight_android -b test/binary.txt -n test/NOTICE.html -c ok")
     nok_result, _ = run_command("fosslight_android -b test/binary.txt -n test/NOTICE.html -c nok")
     divide_result, _ = run_command("fosslight_android -d test/needtoadd-notice.html")
+
 
     # then
     assert help_result is True, "Help command failed"
