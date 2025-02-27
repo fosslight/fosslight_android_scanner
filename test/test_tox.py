6c45adce7b0d86e596d1d9069aed96e922a68e1e
Ethan Lee <ethan.lee@lge.com>
2025-02-27 09:10:05 +0000

    modify test_tox.py
    Signed-off-by: Ethan Lee <ethan.lee@lge.com>

--- test/test_tox.py
+++ test/test_tox.py
@@ -33,7 +33,7 @@ def test_release_environment(run_command):
 
     # when
     help_result, _, _ = run_command("fosslight_android -h")
-    ok_result, _, _ = run_command("fosslight_android -s test/android_12_sample -a test/android_12_sample/android.log")
+    ok_result, _, _ = run_command("fosslight_android -s test/android_12_sample -a android.log")
     
 
     # then
